ce53ed1b2b2bd85fb5c4fd6f3610844dd8bdaaa5
Megan-Woolhouse <146077608+Megan-Woolhouse@users.noreply.github.com>
2023-10-20 15:13:54 +0100

    Added search engine functionality

--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherDisplay from "./WeatherDisplay";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ loaded: false });
-
+  const [city, setCity] = useState(props.defaultCity);
   function displayWeather(response) {
     setWeatherData({
       loaded: true,
       city: response.data.city,
       temperature: response.data.temperature.current,
       description: response.data.condition.description,
-      icon: response.data.condition.icon,
+      imgUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
       wind: response.data.wind.speed,
       humidity: response.data.temperature.humidity,
       date: new Date(response.data.time * 1000),
     });
+  }
+
+  function search() {
+    const apiKey = "f30aff8e88d9o961t06a5249f250f1b6";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function searchCity(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
   }
 
   if (weatherData.loaded) {
@@ -25,12 +40,13 @@ export default function Weather(props) {
         <div className="container shadow">
           <div className="row">
             <div className="col-10">
-              <form>
+              <form onSubmit={searchCity}>
                 <input
                   type="search"
                   name="Search"
                   placeholder="Search City..."
                   className="search-bar"
+                  onChange={updateCity}
                 />
                 <input type="submit" value="Search" className="search-icon" />
               </form>
@@ -39,49 +55,12 @@ export default function Weather(props) {
               <button className="current-location">Current Location</button>
             </div>
           </div>
-          <div className="row mt-3">
-            <div className="col-4">
-              <div className="current-emoji">
-                <img
-                  src={`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${weatherData.icon}.png`}
-                  alt="Weather-icon"
-                  className="current-emoji"
-                />
-              </div>
-              <span className="current">
-                <span className="temp">
-                  {" "}
-                  {Math.round(weatherData.temperature)}{" "}
-                </span>
-
-                <div className="unit">°C</div>
-
-                <div className="current-description">
-                  {weatherData.description}
-                </div>
-              </span>
-            </div>
-
-            <ul className="col-4 wind-humidity">
-              <li>Wind Speed: {weatherData.wind} km/h</li>
-              <li>Humidity: {weatherData.humidity}%</li>
-            </ul>
-
-            <div className="col-4">
-              <h1>{weatherData.city}</h1>
-              <div className="date">
-                <FormattedDate date={weatherData.date} />
-              </div>
-            </div>
-          </div>
-          <div className="forecast"></div>
+          <WeatherDisplay data={weatherData} />
         </div>
       </div>
     );
   } else {
-    const apiKey = "f30aff8e88d9o961t06a5249f250f1b6";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
+    search();
     return "Loading...";
   }
 }
---
+++ src/WeatherDisplay.js
@@ -0,0 +1,37 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherDisplay(props) {
+  return (
+    <div className="row mt-3">
+      <div className="col-4">
+        <div className="current-emoji">
+          <img
+            src={props.data.imgUrl}
+            alt="Weather-icon"
+            className="current-emoji"
+          />
+        </div>
+        <span className="current">
+          <span className="temp"> {Math.round(props.data.temperature)} </span>
+
+          <div className="unit">°C</div>
+
+          <div className="current-description">{props.data.description}</div>
+        </span>
+      </div>
+
+      <ul className="col-4 wind-humidity">
+        <li>Wind Speed: {props.data.wind} km/h</li>
+        <li>Humidity: {props.data.humidity}%</li>
+      </ul>
+
+      <div className="col-4">
+        <h1>{props.data.city}</h1>
+        <div className="date">
+          <FormattedDate date={props.data.date} />
+        </div>
+      </div>
+    </div>
+  );
+}
